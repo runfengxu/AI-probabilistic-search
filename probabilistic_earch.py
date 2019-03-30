f488fc0f3ceb465120f9b987dcb3b94cffa68fa8
Runfeng Xu <34244448+runfengxu@users.noreply.github.com>
2019-03-30 18:59:27 -0400

    Update probabilistic_earch.py

--- probabilistic_earch.py
+++ probabilistic_earch.py
@@ -1,6 +1,5 @@
 import numpy as np
-
-
+import random
 def buildmap():
     nmap=np.random.random((50,50))
     map=[]
@@ -16,18 +15,30 @@ def buildmap():
             else map[i][j]='caves'
 
     return map
-
-
+def generate_target:
+    x=random.randint(0,49)
+    y=random.randint(0,49)
+    return x,y
 
 class Agent(object):
 
     def __init__(self):
         self.belief_state = np.full((50,50),1/2500)
 
-    def search_rule1(self):
-        #search one spot with rule1
-    def search_rule2(self):
-        #search one spot with rule2
+    def search_cell(self,a,b,targetx,targety,map):
+
+        p = random.random()
+        tp= map[a][b]
+        if a==targetx and b== targety:
+            if (tp='flat' && p<0.9) || (tp='hilly'&& p<0.7 )||(tp='forested'&&p<0.3)||(tp='caves'&& p<0.1):
+                return True
+            else:
+                return False
+
+    def rule1(self):
+        return x,y   #return the most likely point by rule1
+    def rule2(self):
+        return x,y   #return the most likely point by rule2
     def update_state(self):
         #update the belief state according to the search result
     def record_state(self):
@@ -39,6 +50,13 @@ class Agent(object):
 
 
 def main():
+    map = buildmap()
+    (x,y)=generate_target()
+    Ai= Agent()
+    a = random.randint(0, 49)
+    b = random.randint(0, 49)
+    Ai.search_cell(a,b,x,y,map)
+
 
 
 
